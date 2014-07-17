42bf917e9d76801c5e543f3cfa2aadfd14427a65
abdulla <abdulla.erkin@hotmail.com>
2014-07-18 01:27:03 +0800

    sqllite
    sqllite sinah kilindi

--- Todo/settings.py
+++ Todo/settings.py
@@ -61,7 +61,8 @@ WSGI_APPLICATION = 'Todo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'katip.sqlite3'),
+        #'NAME': os.path.join(BASE_DIR, 'katip.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'katip.db'),
     }
 }
 
